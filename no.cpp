c07a7fe431d68f942f8432eee2581e8e69abd99d
gujjarafaq570-ops <gujjarafaq570@gmail.com>
2026-03-01 19:40:11 +0500

    Add files via upload

---
+++ no.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int no1,no2,no3,no4,no5,no6,no7,no8,no9,no10,no11,no12,no13,no14,no15,result;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 2:";
+cin>> no2;
+cout<<"Enter number 2:";
+cin>> no2;
+cout<<"Enter number 3:";
+cin>> no3;
+cout<<"Enter number 4:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
+cout<<"Enter number 1:";
+cin>> no1;
